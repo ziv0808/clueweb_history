e9cf017679af82cfb7720fae96fad109124533a2
Ziv <zivos0808@gmail.com>
2019-10-04 22:36:59 +0300

    small fix

--- doc_snapshot_stats_compare.py
+++ doc_snapshot_stats_compare.py
@@ -62,8 +62,8 @@ def calc_tfidf_dict(
         if df_list[i] > 0:
             res_dict[stem_list[i]] = round(tf_list[i]*math.log10(500000000.0/float(df_list[i])), 6)
         else:
-            print(stem_list[i])
-            res_dict[stem_list[i]] = round(tf_list[i] * math.log10(500000000.0 / float(1)), 6)
+
+            res_dict[stem_list[i]] = round(tf_list[i] * math.log10(500000000.0 / float(50000000)), 6)
 
     return res_dict
 
