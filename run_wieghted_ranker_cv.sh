da0481b4f74ab392c7aec35fcfdd0307b1998052
Ziv <zivos0808@gmail.com>
2019-12-16 17:40:54 +0200

    added scrript

--- run_wieghted_ranker_cv.sh
+++ run_wieghted_ranker_cv.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-nohup python python wieghted_ranking_model.py $1 $2 $3 $4 $5 1 20&
-nohup python python wieghted_ranking_model.py $1 $2 $3 $4 $5 21 40&
-nohup python python wieghted_ranking_model.py $1 $2 $3 $4 $5 41 60&
-nohup python python wieghted_ranking_model.py $1 $2 $3 $4 $5 61 80&
-nohup python python wieghted_ranking_model.py $1 $2 $3 $4 $5 81 100&
-nohup python python wieghted_ranking_model.py $1 $2 $3 $4 $5 101 120&
-nohup python python wieghted_ranking_model.py $1 $2 $3 $4 $5 121 140&
-nohup python python wieghted_ranking_model.py $1 $2 $3 $4 $5 141 160&
-nohup python python wieghted_ranking_model.py $1 $2 $3 $4 $5 161 180&
-nohup python python wieghted_ranking_model.py $1 $2 $3 $4 $5 181 200&
+nohup python wieghted_ranking_model.py $1 $2 $3 $4 $5 1 20&
+nohup python wieghted_ranking_model.py $1 $2 $3 $4 $5 21 40&
+nohup python wieghted_ranking_model.py $1 $2 $3 $4 $5 41 60&
+nohup python wieghted_ranking_model.py $1 $2 $3 $4 $5 61 80&
+nohup python wieghted_ranking_model.py $1 $2 $3 $4 $5 81 100&
+nohup python wieghted_ranking_model.py $1 $2 $3 $4 $5 101 120&
+nohup python wieghted_ranking_model.py $1 $2 $3 $4 $5 121 140&
+nohup python wieghted_ranking_model.py $1 $2 $3 $4 $5 141 160&
+nohup python wieghted_ranking_model.py $1 $2 $3 $4 $5 161 180&
+nohup python wieghted_ranking_model.py $1 $2 $3 $4 $5 181 200&
