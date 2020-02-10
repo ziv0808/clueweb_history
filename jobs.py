60fab51eb7176e0ecf593ba90493a980508a443e
Ziv <zivos0808@gmail.com>
2020-02-10 15:14:32 +0200

    added plot func plus removed prev_similarity from stats df

--- jobs.py
+++ jobs.py
@@ -2,6 +2,9 @@ import os
 import sys
 import pandas as pd
 from utils import *
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
 
 def get_relevant_docs_df(
         qurls_path = '/mnt/bi-strg3/v/zivvasilisky/ziv/results/qrels/qrels.adhoc'):
@@ -268,7 +271,7 @@ def create_stats_data_frame_for_snapshot_changes(
                                                       str(clueweb_from_document_stem_diff),per_term_query_ratio_mean,
                                                       len(query_to_stem_mapping[query_num])]
                         next_index += 1
-                prev_interval = interval
+                # prev_interval = interval
     summary_df.to_csv('/mnt/bi-strg3/v/zivvasilisky/ziv/clueweb_history/Summay_snapshot_stats_' + sim_folder_name + '.tsv',
                             sep='\t', index=False)
 
@@ -450,6 +453,170 @@ def create_text_manipulated_interval(
                 f.write(str(res_dict))
 
 
+def create_snapshot_changes_stats(
+        filename='Summay_snapshot_stats_1W.tsv',
+        comp_queries_list=[195, 193, 188, 180, 177, 161, 144, 59, 51, 45, 36, 34, 32, 29, 11, 10,
+                           9, 2, 78, 4, 167, 69, 166, 33, 164, 18, 182, 17, 98, 124, 48],
+        only_relevant_docs=False,
+        for_paper=True):
+    filename_for_save = filename.replace('.tsv', '').replace('Summay_snapshot_stats_', '')
+
+    work_df = pd.read_csv(os.path.join('dfs', filename), sep='\t', index_col=False)
+    if only_relevant_docs == True:
+        rel_df = pd.read_csv('qrels_adhoc_relevant_docs.tsv', sep='\t', index_col=False)
+        rel_df['Relevance'] = rel_df['Relevance'].apply(lambda x: int(x))
+        rel_df = rel_df[rel_df['Relevance'] > 0]
+        work_df = pd.merge(
+            work_df,
+            rel_df[['Docno', 'Query']].rename(columns={'Query': 'QueryNum'}),
+            on=['Docno', 'QueryNum'],
+            how='inner')
+        print("Len after relevant filter " + str(len(work_df)))
+        filename = filename.replace('.tsv', '_Relvant_only.tsv')
+        filename_for_save += '_Relvant_only'
+    # calc ratio measures
+    work_df['QueryTermsRatio'] = work_df.apply(lambda row: row['QueryWords'] / (row['TextLen'] - row['QueryWords']), axis=1)
+    work_df['StopwordsRatio'] = work_df.apply(lambda row: row['#Stopword'] / (row['TextLen'] - row['#Stopword']), axis=1)
+    no_clueweb_df = work_df[work_df['Interval'] != 'ClueWeb09'].copy()
+    clueweb_df = work_df[work_df['Interval'] == 'ClueWeb09'].copy()
+
+    print("Mean ClueWeb09 SimToPrev:")
+    print(clueweb_df[clueweb_df['SimToPrev'].notnull()]['SimToPrev'].mean())
+    print("Mean ClueWeb09 SimToClosePrev:")
+    print(clueweb_df[clueweb_df['SimToClosePrev'].notnull()]['SimToClosePrev'].mean())
+    print("Med ClueWeb09 SimToPrev:")
+    print(clueweb_df[clueweb_df['SimToPrev'].notnull()]['SimToPrev'].median())
+    print("Med ClueWeb09 SimToClosePrev:")
+    print(clueweb_df[clueweb_df['SimToClosePrev'].notnull()]['SimToClosePrev'].median())
+    print("Mean all SimToPrev:")
+    print(work_df[work_df['SimToPrev'].notnull()]['SimToClosePrev'].mean())
+    print("Med all SimToPrev:")
+    print(work_df[work_df['SimToPrev'].notnull()]['SimToPrev'].median())
+    print("Mean not ClueWeb09 SimToClueWeb:")
+    print(no_clueweb_df['SimToClueWeb'].mean())
+    print("Med not ClueWeb09 SimToClueWeb:")
+    print(no_clueweb_df['SimToClueWeb'].median())
+
+    all_docno_and_queries_df = work_df[['Docno', 'QueryNum']].drop_duplicates()
+    only_clueweb_num = 0
+
+    summary_df = pd.DataFrame(
+        columns=['Interval', 'QueryTermsRatio', 'StopwordsRatio', 'Entropy', 'SimClueWeb', 'QueryNum'])
+    next_index = 0
+    exact_mathchs = 0
+    # exact_mathch_df = pd.DataFrame(columns = ['Docno'])
+    # exact_mathch_index = 0
+    for index, row_ in all_docno_and_queries_df.iterrows():
+        doc_df = work_df[work_df['Docno'] == row_['Docno']].copy()
+        doc_df = doc_df[doc_df['QueryNum'] == row_['QueryNum']].copy()
+        if len(doc_df) == 1:
+            only_clueweb_num += 1
+            continue
+        bench_df = doc_df[doc_df['Interval'] == 'ClueWeb09']
+        bench_query_term_ratio = list(bench_df['QueryTermsRatio'])[0]
+        bench_stopword_ratio = list(bench_df['StopwordsRatio'])[0]
+        bench_entropy_ratio = list(bench_df['Entropy'])[0]
+        if bench_stopword_ratio == 0:
+            bench_stopword_ratio = 1.0
+        if bench_query_term_ratio == 0:
+            bench_query_term_ratio = 1.0
+        for index, row in doc_df.iterrows():
+            if row['Interval'] != 'ClueWeb09':
+                interval = row['Interval']
+                query_term_ratio = (row['QueryTermsRatio'] - bench_query_term_ratio) / float(bench_query_term_ratio)
+                stopword_ratio = (row['StopwordsRatio'] - bench_stopword_ratio) / float(bench_stopword_ratio)
+                entropy_ratio = (row['Entropy'] - bench_entropy_ratio) / float(bench_entropy_ratio)
+                sim_cluweb = row['SimToClueWeb']
+                summary_df.loc[next_index] = [interval, query_term_ratio, stopword_ratio, entropy_ratio, sim_cluweb,
+                                              int(row_['QueryNum'])]
+                next_index += 1
+            else:
+                if row['SimToPrev'] == 1.0:
+                    exact_mathchs += 1
+                    # exact_mathch_df.loc[exact_mathch_index] = [row_['Docno']]
+                    # exact_mathch_index += 1
+
+    exact_mathch_df = work_df[work_df['Interval'] != 'ClueWeb09'].copy()
+    exact_mathch_df = exact_mathch_df[exact_mathch_df['SimToClueWeb'] == 1.0][['Docno']].drop_duplicates()
+    exact_mathch_df.to_csv(filename.replace('.tsv', '_Exact_matched_docnos.tsv'), sep='\t', index=False)
+    print("Num of one snapshot:")
+    print(only_clueweb_num)
+    print("Num of Exact Match:")
+    print(len(exact_mathch_df))
+    if 'SIM' in filename:
+        summary_df['Interval'] = summary_df['Interval'].apply(lambda x: 0 if x == 'ClueWeb09' else int(x))
+    summary_df['ExactMatchs'] = summary_df['SimClueWeb'].apply(lambda x: 1 if x == 1.0 else 0)
+    plot_df = summary_df[['Interval', 'ExactMatchs']].groupby(['Interval']).sum()
+    plt.cla()
+    plt.clf()
+    plot_df.plot(kind='bar', color='c', legend=False)
+    plt.xlabel('Interval')
+    plt.tick_params(axis='x', labelsize=5)
+    plt.xticks(rotation=45)
+    plt.ylabel("#Exact Matches")
+    plt.title("#Exact Matches Per Interval")
+    plt.savefig(filename_for_save + "_Exact_Matches_per_interval.png", dpi=300)
+    summary_df['IsCompQuery'] = summary_df['QueryNum'].apply(lambda x: 1 if x in comp_queries_list else 0)
+    interval_quantity_df = summary_df[['Interval', 'QueryNum']].groupby(['Interval']).count()
+    plt.cla()
+    plt.clf()
+    interval_quantity_df.plot(kind='bar', color='r', legend=False)
+    plt.xlabel('Interval')
+    plt.tick_params(axis='x', labelsize=5)
+    plt.xticks(rotation=45)
+    plt.ylabel("#Snapshots")
+    plt.title("#Snapshots Per Interval")
+    plt.savefig(filename_for_save + "_Snapshots_per_interval.png", dpi=300)
+    for measure in ['QueryTermsRatio', 'StopwordsRatio', 'Entropy', 'SimClueWeb']:
+        plt.cla()
+        plt.clf()
+        plot_df = summary_df[['Interval', measure]].groupby(['Interval']).mean()
+        plot_df.rename(columns={measure: 'ALL'}, inplace=True)
+        plot_df_ = summary_df[summary_df['IsCompQuery'] == 1][['Interval', measure]].groupby(['Interval']).mean()
+        plot_df_.rename(columns={measure: 'Comp Queries'}, inplace=True)
+        plot_df = pd.merge(
+            plot_df,
+            plot_df_,
+            left_index=True,
+            right_index=True)
+        plot_df_ = summary_df[summary_df['IsCompQuery'] == 0][['Interval', measure]].groupby(['Interval']).mean()
+        plot_df_.rename(columns={measure: 'Non Comp'}, inplace=True)
+        plot_df = pd.merge(
+            plot_df,
+            plot_df_,
+            left_index=True,
+            right_index=True)
+
+        plot_df.plot(kind='bar')
+        plt.xlabel('Interval')
+        plt.tick_params(axis='x', labelsize=6)
+        plt.xticks(rotation=45)
+        plt.legend(loc="center left", bbox_to_anchor=(1, 0.5))
+        plt.subplots_adjust(right=0.75, bottom=0.15)
+        if measure != 'SimClueWeb':
+            plt.ylabel("%" + measure)
+            plt.title("Mean " + measure + " Precentage Difference from ClueWeb09")
+        else:
+            plt.ylabel(measure)
+            plt.title("Mean " + measure)
+
+        plt.savefig(measure + filename + "_Precentage_Difference_per_interval.png", dpi=300)
+        plot_df.to_csv(os.path.join('plot_df', filename_for_save + '_' + measure + '.tsv'), sep='\t')
+        if for_paper == True:
+            plt.cla()
+            plt.clf()
+            if measure == 'SimClueWeb':
+                tmp_df = plot_df.reset_index()
+                # plot_df[['ALL']].plot(kind = 'bar', color = 'c', legend=False)
+                plt.plot(pd.to_datetime(tmp_df['Interval']), tmp_df['ALL'], color='c')
+                plt.xlabel('Interval')
+                plt.subplots_adjust(bottom=0.15)
+            else:
+                plot_df[['ALL']].plot(kind='bar', color='c', legend=False)
+                plt.xlabel('Snapshot Number')
+            plt.tick_params(axis='x', labelsize=6)
+            plt.xticks(rotation=45)
+            plt.savefig(measure + filename + "_Precentage_Difference_per_interval_For_Paper.png", dpi=300)
 
 
 if __name__ == '__main__':
