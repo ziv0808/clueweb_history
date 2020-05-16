ade1ead6ae41732b0119700c8210823fee034f29
Ziv <zivos0808@gmail.com>
2020-05-16 20:19:20 +0300

    added indexer

--- warcer_for_publish.py
+++ warcer_for_publish.py
@@ -215,42 +215,89 @@ def create_warc_files_for_time_interval(
 
     return num_of_records_in_interval, lost_records
 
+def gzip_all_folder(
+        folder_name):
+
+    for inner_folder in os.listdir(folder_name):
+        print (inner_folder)
+        for file_name in os.listdir(os.path.join(folder_name, inner_folder)):
+            if not file_name.endswith('.gz'):
+                print(os.path.join(os.path.join(folder_name, inner_folder), file_name))
+                res = subprocess.check_call(['gzip', os.path.join(os.path.join(folder_name, inner_folder), file_name)])
+
 if __name__ == '__main__':
     work_year = "2008"
     init_interval_month = 1
     end_interval_month = 12
-    print("Run Year: " + str(work_year))
-    print("From interval month: " + str(init_interval_month))
-    print("Till interval month: " + str(end_interval_month))
-    sys.stdout.flush()
-    interval_list = build_interval_list(
-        work_year=work_year,
-        frequency='1M',
-        start_month=int(init_interval_month),
-        end_month=int(end_interval_month))
-
-    destination_folder = "/mnt/bi-strg3/v/zivvasilisky/data/HSCW09/"
-    data_folder ="/mnt/bi-strg3/v/zivvasilisky/ziv/data/retrived_htmls/2008/"
-    relevant_docnos_file = "/mnt/bi-strg3/v/zivvasilisky/ziv/data/50_per_q/all_urls_no_spam_filtered.tsv"
-    universe_df = pd.read_csv(relevant_docnos_file, sep = '\t', index_col = False)
-    universe_list = list(universe_df['Docno'].drop_duplicates())
-
-    summary_df = pd.DataFrame(columns = ['Interval', 'NumOfDocs', 'LostRecords'])
-    next_index = 0
-    for interval in interval_list:
-        print ("Curr interval: " + str(interval))
+    action = sys.argv[1]
+    if action == "Warcer":
+        print("Run Year: " + str(work_year))
+        print("From interval month: " + str(init_interval_month))
+        print("Till interval month: " + str(end_interval_month))
         sys.stdout.flush()
-        if not os.path.exists(os.path.join(destination_folder, interval)):
-            os.mkdir(os.path.join(destination_folder, interval))
-
-        num_records, lost_records = create_warc_files_for_time_interval(
-            destination_folder=destination_folder,
-            time_interval=interval,
-            data_folder=data_folder,
+        interval_list = build_interval_list(
             work_year=work_year,
-            universe_list=universe_list)
+            frequency='1M',
+            start_month=int(init_interval_month),
+            end_month=int(end_interval_month))
 
-        summary_df.loc[next_index] = [interval, num_records, lost_records]
-        next_index += 1
+        destination_folder = "/mnt/bi-strg3/v/zivvasilisky/data/HSCW09/"
+        data_folder ="/mnt/bi-strg3/v/zivvasilisky/ziv/data/retrived_htmls/2008/"
+        relevant_docnos_file = "/mnt/bi-strg3/v/zivvasilisky/ziv/data/50_per_q/all_urls_no_spam_filtered.tsv"
+        universe_df = pd.read_csv(relevant_docnos_file, sep = '\t', index_col = False)
+        universe_list = list(universe_df['Docno'].drop_duplicates())
 
-    summary_df.to_csv(os.path.join(data_folder, 'Summry_warcer.tsv'), sep = '\t', index = False)
+        summary_df = pd.DataFrame(columns = ['Interval', 'NumOfDocs', 'LostRecords'])
+        next_index = 0
+        for interval in interval_list:
+            print ("Curr interval: " + str(interval))
+            sys.stdout.flush()
+            if not os.path.exists(os.path.join(destination_folder, interval)):
+                os.mkdir(os.path.join(destination_folder, interval))
+
+            num_records, lost_records = create_warc_files_for_time_interval(
+                destination_folder=destination_folder,
+                time_interval=interval,
+                data_folder=data_folder,
+                work_year=work_year,
+                universe_list=universe_list)
+
+            summary_df.loc[next_index] = [interval, num_records, lost_records]
+            next_index += 1
+
+        summary_df.to_csv(os.path.join(data_folder, 'Summry_warcer.tsv'), sep = '\t', index = False)
+    elif action == "Indexer":
+        print("Run Year: " + str(work_year))
+        print("From interval month: " + str(init_interval_month))
+        print("Till interval month: " + str(end_interval_month))
+        sys.stdout.flush()
+        interval_list = build_interval_list(
+            work_year=work_year,
+            frequency='1M',
+            start_month=int(init_interval_month),
+            end_month=int(end_interval_month))
+
+        destination_folder = "/mnt/bi-strg3/v/zivvasilisky/index/HSCW09/"
+        data_folder = "/mnt/bi-strg3/v/zivvasilisky/data/HSCW09/"
+
+        for interval in interval_list:
+            print("Curr interval: " + str(interval))
+            sys.stdout.flush()
+            gzip_all_folder(os.path.join(data_folder, interval))
+            print('Gziped all...')
+            sys.stdout.flush()
+            with open('/mnt/bi-strg3/v/zivvasilisky/ziv/clueweb_history/IndriBuildIndex.xml', 'r') as f:
+                params_text = f.read()
+
+            with open('/mnt/bi-strg3/v/zivvasilisky/ziv/clueweb_history/Index_params/IndriBuildIndex_' + str(
+                    interval) + '.xml',
+                      'w') as f:
+                f.write(params_text.replace('###', str(os.path.join(destination_folder, interval))).replace('%%%', str(
+                    os.path.join(data_folder, interval))))
+            print('Params fixed...')
+            sys.stdout.flush()
+            res = subprocess.check_call(['/mnt/bi-strg3/v/zivvasilisky/ziv/env/indri/indri/bin/IndriBuildIndex',
+                                         '/mnt/bi-strg3/v/zivvasilisky/ziv/clueweb_history/Index_params/IndriBuildIndex_' + str(
+                                             interval) + '.xml'])
+            print('Index built...')
+            sys.stdout.flush()
