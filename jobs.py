4e40015a34a13a0ef52db983e9bce1fdd9f9b9ee
Ziv <zivos0808@gmail.com>
2020-07-24 22:44:41 +0300

    smal fix

--- jobs.py
+++ jobs.py
@@ -1494,11 +1494,11 @@ def handle_rank_svm_params(
 def asrc_data_parser(
         filepath,
         rel_filepath):
-
+    doc_df = pd.DataFrame(columns=['Docno', 'QueryNum', 'Interval'])
     processed_docs_folder = '/mnt/bi-strg3/v/zivvasilisky/ziv/data/processed_document_vectors/asrc/2008/'
     with open(filepath, 'r') as f:
         soup = BeautifulSoup(f.read())
-
+    doc_df = pd.DataFrame(columns=['Docno', 'QueryNum', 'Interval'])
     stemmer = Stemmer()
     big_doc_index = {}
     stopword_list = get_stopword_list()
@@ -1507,7 +1507,7 @@ def asrc_data_parser(
     for index, row in df_query_stems.iterrows():
         query_num = ('0' * (3 - len(str(row['QueryNum'])))) + str(row['QueryNum'])
         query_to_stem_mapping[query_num] = convert_query_to_tf_dict(row['QueryStems'])
-
+    doc_df = pd.DataFrame(columns=['Docno', 'QueryNum', 'Interval'])
     all_docs = soup.find_all('doc')
     cc_dict = {'ALL_TERMS_COUNT' : 0,
                'ALL_SW_COUNT'    : 0}
@@ -1662,10 +1662,8 @@ def asrc_data_parser(
                     res_dict[diff]= big_doc_index[query_user_str][additional_round]['json']
             with open(os.path.join(os.path.join(processed_docs_folder, 'SIM'), docno +'.json'), 'w') as f:
                 f.write(str(res_dict))
-            col_list = ['Docno', 'QueryNum', 'Interval'] + base_feature_list
-            curr_doc_df = pd.DataFrame({})
-            for col in col_list:
-                curr_doc_df[col] = None
+
+            curr_doc_df = pd.DataFrame(columns=['Docno', 'QueryNum', 'Interval'] + base_feature_list)
             tmp_idx = 0
             for i in list(reversed(range(round_))):
                 if i == 0:
