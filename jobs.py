46373f9006f30e2a4416efcaaf58c23bc69c1aa5
Ziv <zivos0808@gmail.com>
2020-10-04 20:12:50 +0300

    added rhs to asrc unite func

--- jobs.py
+++ jobs.py
@@ -1838,7 +1838,7 @@ def unite_asrc_data_results(
             feat_group = filename.replace(inner_fold.split('/')[-1] + '_MinMax_', '').replace('.txt', '').replace('_AllByMonths', '').replace('_', '+')
             round_res_dict[round_][feat_group.replace('_', '+')] = tmp_res_dict
             print(feat_group)
-            if dataset_name == 'herd_control':
+            if dataset_name == 'herd_control' and 59 in tmp_res_dict:
                 del tmp_res_dict[59]
             if feat_group.replace('_', '+') in big_res_dict:
                 print(num_files)
