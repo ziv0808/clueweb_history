30bd33d58add1433d88ca96b8df5c1091fd08e28
Ziv <zivos0808@gmail.com>
2020-11-05 22:41:50 +0200

    fix permutation test

--- utils.py
+++ utils.py
@@ -831,7 +831,7 @@ def powerset(iterable):
 
 
 
-def pemutation_test(test_group, control_group, total_number=100000):
+def pemutation_test(test_group, control_group, total_number=10000):
     """
 
     :param test_group:  vector of test group results (originally sorted by qid)
