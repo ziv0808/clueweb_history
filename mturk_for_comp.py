ea809c103059a12d5ee127d8566d672ed0a02084
Ziv <zivos0808@gmail.com>
2020-12-07 17:57:41 +0200

    added module

--- mturk_for_comp.py
+++ mturk_for_comp.py
@@ -1,9 +1,11 @@
 import boto3
 import xmltodict
 
+with open('.ssh/params.txt', 'r') as f:
+    params = f.read().split('\n')
 region_name = 'us-east-1'
-aws_access_key_id = 'AKIAI4XYPCUMAVRODWBA'
-aws_secret_access_key = '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'
+aws_access_key_id = params[0]
+aws_secret_access_key = params[1]
 
 endpoint_url = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
 
