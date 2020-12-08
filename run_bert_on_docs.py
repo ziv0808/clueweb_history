46a96844d59b24a3d209280993867ddeae884225
Ziv <zivos0808@gmail.com>
2020-12-09 00:36:34 +0200

    added concat models

--- run_bert_on_docs.py
+++ run_bert_on_docs.py
@@ -42,6 +42,7 @@ def get_trec_prepared_df_form_res_df(
 
 if __name__=="__main__":
     inner_fold = sys.argv[1]
+    operation = sys.argv[2]
 
     with open('/mnt/bi-strg3/v/zivvasilisky/ziv/data/' + inner_fold + '/RawData.json', 'r') as f:
         big_doc_index = ast.literal_eval(f.read())
@@ -59,39 +60,91 @@ if __name__=="__main__":
     model = AutoModelForSequenceClassification.from_pretrained("amberoad/bert-multilingual-passage-reranking-msmarco")
 
     per_round_res_df_dict = {}
-    for query_user in big_doc_index:
-        query = query_user.split('-')[0]
-        print(query_user)
-        sys.stdout.flush()
-        for round_ in big_doc_index[query_user]:
-            if round_ not in per_round_res_df_dict:
-                per_round_res_df_dict[round_] = {}
-                per_round_res_df_dict[round_]['Df'] = pd.DataFrame(columns = ['QueryNum','Docno', 'Score'])
-                per_round_res_df_dict[round_]['Idx'] = 0
-            big_doc_index[query_user][round_]['json']['BERTScore'] = get_query_doc_rel_proba(
-                                                        tokenizer=tokenizer,
-                                                        model=model,
-                                                        query=query_num_to_text[query],
-                                                        document=big_doc_index[query_user][round_]['json']['Fulltext'])
-            docno = "EPOCH-" + str(round_).zfill(2) + '-' + query_user
-            print(docno)
+    if operation == 'BaseModel':
+        for query_user in big_doc_index:
+            query = query_user.split('-')[0]
+            print(query_user)
             sys.stdout.flush()
-            per_round_res_df_dict[round_]['Df'].loc[per_round_res_df_dict[round_]['Idx']] = [query, docno, big_doc_index[query_user][round_]['json']['BERTScore']]
-            per_round_res_df_dict[round_]['Idx'] += 1
+            for round_ in big_doc_index[query_user]:
+                if round_ not in per_round_res_df_dict:
+                    per_round_res_df_dict[round_] = {}
+                    per_round_res_df_dict[round_]['Df'] = pd.DataFrame(columns = ['QueryNum','Docno', 'Score'])
+                    per_round_res_df_dict[round_]['Idx'] = 0
+                big_doc_index[query_user][round_]['json']['BERTScore'] = get_query_doc_rel_proba(
+                                                            tokenizer=tokenizer,
+                                                            model=model,
+                                                            query=query_num_to_text[query],
+                                                            document=big_doc_index[query_user][round_]['json']['Fulltext'])
+                docno = "EPOCH-" + str(round_).zfill(2) + '-' + query_user
+                print(docno)
+                sys.stdout.flush()
+                per_round_res_df_dict[round_]['Df'].loc[per_round_res_df_dict[round_]['Idx']] = [query, docno, big_doc_index[query_user][round_]['json']['BERTScore']]
+                per_round_res_df_dict[round_]['Idx'] += 1
 
-    with open('/mnt/bi-strg3/v/zivvasilisky/ziv/data/' + inner_fold + '/RawDataWithBERT.json', 'w') as f:
-        big_doc_index = f.write(str(big_doc_index))
+        with open('/mnt/bi-strg3/v/zivvasilisky/ziv/data/' + inner_fold + '/RawDataWithBERT.json', 'w') as f:
+            big_doc_index = f.write(str(big_doc_index))
 
-    save_folder = '/mnt/bi-strg3/v/zivvasilisky/ziv/results/bert/'
-    for round_ in per_round_res_df_dict:
-        df = per_round_res_df_dict[round_]['Df']
-        score_df = get_trec_prepared_df_form_res_df(df, 'Score')
-        save_filename = inner_fold + '_' + str(round_).zfill(2)+'_BERT_Results.txt'
-        with open(os.path.join(save_folder, save_filename), 'w') as f:
-            big_doc_index = f.write(convert_df_to_trec(score_df))
-    
+        save_folder = '/mnt/bi-strg3/v/zivvasilisky/ziv/results/bert/'
+        for round_ in per_round_res_df_dict:
+            df = per_round_res_df_dict[round_]['Df']
+            score_df = get_trec_prepared_df_form_res_df(df, 'Score')
+            save_filename = inner_fold + '_' + str(round_).zfill(2)+'_BERT_Results.txt'
+            with open(os.path.join(save_folder, save_filename), 'w') as f:
+                big_doc_index = f.write(convert_df_to_trec(score_df))
 
+    if operation == 'ConcatModel':
+        for query_user in big_doc_index:
+            query = query_user.split('-')[0]
+            print(query_user)
+            sys.stdout.flush()
+            for round_ in big_doc_index[query_user]:
+                if int(round_) > 1:
+                    if round_ not in per_round_res_df_dict:
+                        per_round_res_df_dict[round_] = {'Inc' : {}, 'Dec' :{}}
+                        per_round_res_df_dict[round_]['Inc']['Df'] = pd.DataFrame(columns=['QueryNum', 'Docno', 'Score'])
+                        per_round_res_df_dict[round_]['Dec']['Df'] = pd.DataFrame(columns=['QueryNum', 'Docno', 'Score'])
+                        per_round_res_df_dict[round_]['Idx'] = 0
 
+                    if round_ == '02':
+                        fulltext_inc = big_doc_index[query_user]['01']['json']['Fulltext'] + '\n' + big_doc_index[query_user][round_]['json']['Fulltext']
+                        fulltext_dec = big_doc_index[query_user][round_]['json']['Fulltext'] + '\n' + big_doc_index[query_user]['01']['json']['Fulltext']
+                    else:
+                        fulltext_inc = big_doc_index[query_user][str(int(round_) - 2).zfill(2)]['json']['Fulltext'] + '\n' + \
+                                       big_doc_index[query_user][str(int(round_) - 1).zfill(2)]['json']['Fulltext'] + '\n' + \
+                                       big_doc_index[query_user][round_]['json']['Fulltext']
+                        fulltext_dec = big_doc_index[query_user][round_]['json']['Fulltext'] + '\n' + \
+                                       big_doc_index[query_user][str(int(round_) - 1).zfill(2)]['json']['Fulltext'] + '\n' + \
+                                       big_doc_index[query_user][str(int(round_) - 2).zfill(2)]['json']['Fulltext']
+                    score_inc = get_query_doc_rel_proba(
+                        tokenizer=tokenizer,
+                        model=model,
+                        query=query_num_to_text[query],
+                        document=fulltext_inc)
+                    score_dec = get_query_doc_rel_proba(
+                        tokenizer=tokenizer,
+                        model=model,
+                        query=query_num_to_text[query],
+                        document=fulltext_dec)
+                    docno = "EPOCH-" + str(round_).zfill(2) + '-' + query_user
+                    print(docno)
+                    sys.stdout.flush()
+                    per_round_res_df_dict[round_]['Inc']['Df'].loc[per_round_res_df_dict[round_]['Idx']] = [query, docno, score_inc]
+                    per_round_res_df_dict[round_]['Dec']['Df'].loc[per_round_res_df_dict[round_]['Idx']] = [query, docno, score_dec]
+                    per_round_res_df_dict[round_]['Idx'] += 1
+
+        save_folder = '/mnt/bi-strg3/v/zivvasilisky/ziv/results/bert/'
+        for round_ in per_round_res_df_dict:
+            df = per_round_res_df_dict[round_]['Inc']['Df']
+            score_df = get_trec_prepared_df_form_res_df(df, 'Score')
+            save_filename = inner_fold + '_' + str(round_).zfill(2) + '_BERT_Concat_Inc_Results.txt'
+            with open(os.path.join(save_folder, save_filename), 'w') as f:
+                big_doc_index = f.write(convert_df_to_trec(score_df))
+
+            df = per_round_res_df_dict[round_]['Dec']['Df']
+            score_df = get_trec_prepared_df_form_res_df(df, 'Score')
+            save_filename = inner_fold + '_' + str(round_).zfill(2) + '_BERT_Concat_Dec_Results.txt'
+            with open(os.path.join(save_folder, save_filename), 'w') as f:
+                big_doc_index = f.write(convert_df_to_trec(score_df))
 
 
 
