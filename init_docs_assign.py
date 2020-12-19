b8f76566c9b0f136b422a8ecd363aa66d9331255
Ziv <zivos0808@gmail.com>
2020-12-19 19:41:30 +0200

    6 th round checks

--- init_docs_assign.py
+++ init_docs_assign.py
@@ -384,15 +384,21 @@ queries = list(data.keys())
 # print("Curr Round VS Zero:")
 # compare_doc_files_vs_db(data,is_first=True)
 
-data_round_4 = read_current_doc_file('/lv_local/home/zivvasilisky/ASR20/epoch_run/Collections/TrecText/2020-12-02-22-02-13-998936')
-rank_round_4 = parse_res_file('/lv_local/home/zivvasilisky/ASR20/epoch_run/Results/RankedLists/LambdaMART2020-12-02-22-02-13-998936')
+# data_round_4 = read_current_doc_file('/lv_local/home/zivvasilisky/ASR20/epoch_run/Collections/TrecText/2020-12-02-22-02-13-998936')
+# rank_round_4 = parse_res_file('/lv_local/home/zivvasilisky/ASR20/epoch_run/Results/RankedLists/LambdaMART2020-12-02-22-02-13-998936')
+#
+# print("Curr Round VS 4TH:")
+# compare_doc_files_vs_db(data_round_4,is_first=False,rank_dict=rank_round_4)
+# print("Curr Round VS Zero:")
+# compare_doc_files_vs_db(data,is_first=True)
 
-print("Curr Round VS 4TH:")
-compare_doc_files_vs_db(data_round_4,is_first=False,rank_dict=rank_round_4)
+data_round_5 = read_current_doc_file('/lv_local/home/zivvasilisky/ASR20/epoch_run/Collections/TrecText/2020-12-09-22-44-03-416874')
+rank_round_5 = parse_res_file('/lv_local/home/zivvasilisky/ASR20/epoch_run/Results/RankedLists/LambdaMART2020-12-09-22-44-03-416874')
+
+print("Curr Round VS 5TH:")
+compare_doc_files_vs_db(data_round_5,is_first=False,rank_dict=rank_round_5)
 print("Curr Round VS Zero:")
 compare_doc_files_vs_db(data,is_first=True)
-
-
 # while True:
 #     mapping, query_user_map = user_query_mapping_z(
 #         users=users,
