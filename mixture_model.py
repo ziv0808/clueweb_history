fd7a3419b63c988fcd947bf1bff1ce5febf91390
Ziv <zivos0808@gmail.com>
2020-12-26 19:51:09 +0200

    added regular lms and separated hyper param learning

--- mixture_model.py
+++ mixture_model.py
@@ -90,7 +90,9 @@ def get_kl_result_for_doc(
         query_stem_dict,
         doc_proba_dict,
         collection_dict,
-        beta):
+        params,
+        model_to_run,
+        doc_len):
 
     kl_score = 0.0
     work_stem_list = list(query_stem_dict.keys())
@@ -103,13 +105,23 @@ def get_kl_result_for_doc(
             stem_d_proba = doc_proba_dict[stem]
         else:
             stem_d_proba = 0.0
-
+        if 'JM' in model_to_run:
+            beta = params['Beta']
+        elif 'DIR' in model_to_run:
+            beta = (1.0 - (params['Mue']/ float(params['Mue'] + doc_len)))
         stem_d_proba = beta*stem_d_proba + (1.0-beta)*(float(collection_count_for_word) / collection_len)
         query_tf = query_stem_dict[stem]
         stem_q_prob = float(query_tf) / q_normalizer
         kl_score += (-1) * stem_q_prob * (math.log((stem_q_prob / stem_d_proba), 2))
 
     return kl_score
+
+def create_mle_dict_for_doc(
+        doc_dict):
+    res_dict = {}
+    for stem in doc_dict['TfDict']:
+        res_dict[stem] = float(doc_dict['TfDict'][stem]) / float(doc_dict['NumWords'])
+    return res_dict
 
 def get_scored_df_for_query(
         query_num,
@@ -118,7 +130,8 @@ def get_scored_df_for_query(
         query_adveserial_dict,
         collection_dict,
         processed_docs_path,
-        params):
+        params,
+        model_to_run):
 
     res_df = pd.DataFrame(columns=['Query_ID', 'Iteration', 'Docno', 'Rank', 'Score', 'Method'])
     next_index = 0
@@ -129,17 +142,24 @@ def get_scored_df_for_query(
         with open(os.path.join(processed_docs_path, docno + '.json'), 'r') as f:
             doc_dict = ast.literal_eval(f.read())
         doc_dict = doc_dict['ClueWeb09']
-        doc_proba_dict = run_em_for_doc(
-            doc_dict=doc_dict,
-            adveserial_dict=query_adveserial_dict,
-            lambda_1=params['Lambda1'],
-            collection_dict=collection_dict,
-            lambda_2=params['Lambda2'])
+        if 'Mixture' in model_to_run:
+            doc_proba_dict = run_em_for_doc(
+                doc_dict=doc_dict,
+                adveserial_dict=query_adveserial_dict,
+                lambda_1=params['Lambda1'],
+                collection_dict=collection_dict,
+                lambda_2=params['Lambda2'])
+        elif model_to_run in ['JM', 'DIR']:
+            doc_proba_dict = create_mle_dict_for_doc(doc_dict)
+
         doc_score = get_kl_result_for_doc(
             query_stem_dict=query_dict,
             doc_proba_dict=doc_proba_dict,
             collection_dict=collection_dict,
-            beta=params['Beta'])
+            beta=params,
+            model_to_run=model_to_run,
+            doc_len=doc_dict['NumWords'])
+
 
         res_df.loc[next_index] = ["0" * (3 - len(str(query_num))) + str(query_num), 'Q0', docno, 0, doc_score, 'indri']
         next_index += 1
@@ -155,7 +175,8 @@ def test_queries(
     all_query_adveserial_dict,
     collection_dict,
     params,
-    processed_docs_path):
+    processed_docs_path,
+    model_to_run):
 
     big_df = pd.DataFrame({})
     for index, row in stemmed_queries_df.iterrows():
@@ -171,7 +192,8 @@ def test_queries(
             collection_dict=collection_dict,
             query_adveserial_dict=all_query_adveserial_dict[str(query_num).zfill(3)],
             params=params,
-            processed_docs_path=processed_docs_path)
+            processed_docs_path=processed_docs_path,
+            model_to_run=model_to_run)
 
         big_df = big_df.append(res_df, ignore_index=True)
 
@@ -302,8 +324,8 @@ def get_score_retrieval_score_for_df(
     return res_dict
 
 if __name__=='__main__':
-    inner_fold = sys.argv[1]
-    adverserial_method = sys.argv[2]
+    model_to_run = sys.argv[1]
+    inner_fold = sys.argv[2]
     train_leave_one_out = ast.literal_eval(sys.argv[3])
 
     sw_rmv = True
@@ -322,7 +344,6 @@ if __name__=='__main__':
         qrel_filepath = '/mnt/bi-strg3/v/zivvasilisky/ziv/results/qrels/curr_comp.rel'
 
     processed_docs_folder = '/mnt/bi-strg3/v/zivvasilisky/ziv/data/processed_document_vectors/'+inner_fold+'/2008/SIM/'
-    save_folder = '/mnt/bi-strg3/v/zivvasilisky/ziv/results/mixture_model_res/'
 
     query_to_doc_mapping_df = create_query_to_doc_mapping_df(inner_fold=inner_fold)
     limited_q_list = list(query_to_doc_mapping_df['QueryNum'].drop_duplicates())
@@ -331,40 +352,79 @@ if __name__=='__main__':
     all_fold_params_summary = "Fold" + '\t' + "Params" + '\n'
 
     q_list, fold_list = get_asrc_q_list_and_fold_list(inner_fold, train_leave_one_out=train_leave_one_out)
-
-    beta_option_list = [0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-    lambda1_option_list = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-    lambda2_option_list = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
-
     cc_dict = create_cc_dict(dataset_name=datset_name)
-    adverserial_dict = make_adverserial_dict_by_method(
-        dataset_name=datset_name,
-        curr_round=asrc_round,
-        adverserial_method=adverserial_method)
 
-    for fold in fold_list:
-        start_test_q = int(fold[0])
-        end_test_q = int(fold[1])
-        affix = inner_fold + '_' + str(start_test_q) + '_' + str(end_test_q) + "_" + adverserial_method
+    if 'Mixture' in model_to_run:
+        adverserial_method = sys.argv[4]
+        save_folder = '/mnt/bi-strg3/v/zivvasilisky/ziv/results/mixture_model_res/'
+        if 'JM' in model_to_run:
+            beta_option_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+        elif 'DIR' in model_to_run:
+            beta_option_list = [50, 100, 200, 300, 500, 700, 800, 900, 1000, 1200, 1500]
 
-        stemmed_queries_df = create_stemmed_queries_df(sw_rmv=sw_rmv, limited_q_list=limited_q_list)
+        lambda1_option_list = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+        lambda2_option_list = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
 
-        test_set_q = list(range(start_test_q, end_test_q + 1))
-        stemmed_queries_df['IsTest'] = stemmed_queries_df['QueryNum'].apply(lambda x: 1 if x in test_set_q else 0)
-        test_queries_df = stemmed_queries_df[stemmed_queries_df['IsTest'] == 1].copy()
-        train_queries_df = stemmed_queries_df[stemmed_queries_df['IsTest'] == 0]
+        adverserial_dict = make_adverserial_dict_by_method(
+            dataset_name=datset_name,
+            curr_round=asrc_round,
+            adverserial_method=adverserial_method)
 
-        best_config_dict = None
-        best_ndcg = 0.0
+        for fold in fold_list:
+            start_test_q = int(fold[0])
+            end_test_q = int(fold[1])
+            affix = inner_fold + '_' + str(start_test_q) + '_' + str(end_test_q) + "_" + adverserial_method + "_" + model_to_run
 
-        for beta in beta_option_list:
+            stemmed_queries_df = create_stemmed_queries_df(sw_rmv=sw_rmv, limited_q_list=limited_q_list)
+
+            test_set_q = list(range(start_test_q, end_test_q + 1))
+            stemmed_queries_df['IsTest'] = stemmed_queries_df['QueryNum'].apply(lambda x: 1 if x in test_set_q else 0)
+            test_queries_df = stemmed_queries_df[stemmed_queries_df['IsTest'] == 1].copy()
+            train_queries_df = stemmed_queries_df[stemmed_queries_df['IsTest'] == 0]
+
+            best_config_dict = None
+            best_ndcg = 0.0
+            init_lam_1 = 0.0
+            init_lam_2 = 0.0
+            for beta in beta_option_list:
+                params = {'Lambda1': init_lam_1,
+                          'Lambda2': init_lam_2}
+                if 'JM' in model_to_run:
+                    params['Beta'] = beta
+                elif 'DIR' in model_to_run:
+                    params['Mue'] = beta
+                print(params)
+                big_df = test_queries(
+                    stemmed_queries_df=train_queries_df,
+                    query_to_doc_mapping_df=query_to_doc_mapping_df,
+                    all_query_adveserial_dict=adverserial_dict,
+                    collection_dict=cc_dict,
+                    params=params,
+                    processed_docs_path=processed_docs_folder,
+                    model_to_run=model_to_run)
+
+                res_dict = get_score_retrieval_score_for_df(
+                    affix=affix,
+                    big_df=big_df,
+                    qrel_filepath=qrel_filepath,
+                    save_folder=save_folder)
+
+                if res_dict['all']['NDCG@5'] > best_ndcg:
+                    print(affix + " " + "SCORE : " + str(res_dict['all']))
+                    best_ndcg = res_dict['all']['NDCG@5']
+                    best_config_dict = params.copy()
+                    print(best_config_dict)
+
             for lambda1 in lambda1_option_list:
                 for lambda2 in lambda2_option_list:
                     if (lambda1 + lambda2) >= 1:
                         continue
-                    params = {'Beta'    : beta,
-                              'Lambda1' : lambda1,
-                              'Lambda2' : lambda2}
+                    params = {'Lambda1': lambda1,
+                              'Lambda2': lambda2}
+                    if 'JM' in model_to_run:
+                        params['Beta'] = best_config_dict['Beta']
+                    elif 'DIR' in model_to_run:
+                        params['Mue'] = best_config_dict['Mue']
                     print(params)
                     big_df = test_queries(
                                 stemmed_queries_df=train_queries_df,
@@ -372,7 +432,8 @@ if __name__=='__main__':
                                 all_query_adveserial_dict=adverserial_dict,
                                 collection_dict=cc_dict,
                                 params=params,
-                                processed_docs_path=processed_docs_folder)
+                                processed_docs_path=processed_docs_folder,
+                                model_to_run=model_to_run)
 
                     res_dict = get_score_retrieval_score_for_df(
                         affix=affix,
@@ -386,22 +447,96 @@ if __name__=='__main__':
                         best_config_dict = params.copy()
                         print(best_config_dict)
 
-        test_fold_df = test_queries(
-                                stemmed_queries_df=test_queries_df,
-                                query_to_doc_mapping_df=query_to_doc_mapping_df,
-                                all_query_adveserial_dict=adverserial_dict,
-                                collection_dict=cc_dict,
-                                params=best_config_dict,
-                                processed_docs_path=processed_docs_folder)
+            test_fold_df = test_queries(
+                                    stemmed_queries_df=test_queries_df,
+                                    query_to_doc_mapping_df=query_to_doc_mapping_df,
+                                    all_query_adveserial_dict=adverserial_dict,
+                                    collection_dict=cc_dict,
+                                    params=best_config_dict,
+                                    processed_docs_path=processed_docs_folder,
+                                    model_to_run=model_to_run)
 
-        all_folds_df = all_folds_df.append(test_fold_df, ignore_index=True)
-        all_fold_params_summary += str(start_test_q) + '_' + str(end_test_q) + '\t' + str(best_config_dict) + '\n'
+            all_folds_df = all_folds_df.append(test_fold_df, ignore_index=True)
+            all_fold_params_summary += str(start_test_q) + '_' + str(end_test_q) + '\t' + str(best_config_dict) + '\n'
 
-    filenam_addition = ""
-    if train_leave_one_out == True:
-        filenam_addition += "_LoO"
-    curr_file_name = inner_fold + '_' + adverserial_method + filenam_addition + "_Results.txt"
-    with open(os.path.join(save_folder + 'final_res/', curr_file_name), 'w') as f:
-        f.write(convert_df_to_trec(all_folds_df))
-    with open(os.path.join(save_folder + 'final_res/', curr_file_name.replace('_Results', '_Params')), 'w') as f:
-        f.write(all_fold_params_summary)
+        filenam_addition = ""
+        if train_leave_one_out == True:
+            filenam_addition += "_LoO"
+        curr_file_name = inner_fold + '_' + adverserial_method + filenam_addition + "_Results.txt"
+        with open(os.path.join(save_folder + 'final_res/', curr_file_name), 'w') as f:
+            f.write(convert_df_to_trec(all_folds_df))
+        with open(os.path.join(save_folder + 'final_res/', curr_file_name.replace('_Results', '_Params')), 'w') as f:
+            f.write(all_fold_params_summary)
+
+    else:
+        save_folder = '/mnt/bi-strg3/v/zivvasilisky/ziv/results/basic_lm/'
+        if model_to_run == 'JM':
+            beta_option_list = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+        elif model_to_run == 'DIR':
+            beta_option_list = [50, 100, 200, 300, 500, 700, 800, 900, 1000, 1200, 1500, 2000]
+        for fold in fold_list:
+            start_test_q = int(fold[0])
+            end_test_q = int(fold[1])
+            affix = inner_fold + '_' + str(start_test_q) + '_' + str(end_test_q) + "_" + model_to_run
+
+            stemmed_queries_df = create_stemmed_queries_df(sw_rmv=sw_rmv, limited_q_list=limited_q_list)
+
+            test_set_q = list(range(start_test_q, end_test_q + 1))
+            stemmed_queries_df['IsTest'] = stemmed_queries_df['QueryNum'].apply(lambda x: 1 if x in test_set_q else 0)
+            test_queries_df = stemmed_queries_df[stemmed_queries_df['IsTest'] == 1].copy()
+            train_queries_df = stemmed_queries_df[stemmed_queries_df['IsTest'] == 0]
+
+            # mock adverserial dict
+            adverserial_dict = make_adverserial_dict_by_method(
+                dataset_name=datset_name,
+                curr_round=asrc_round,
+                adverserial_method='PrevWinner')
+
+            best_config_dict = None
+            best_ndcg = 0.0
+            for beta in beta_option_list:
+                params ={}
+                if 'JM' in model_to_run:
+                    params['Beta'] = beta
+                elif 'DIR' in model_to_run:
+                    params['Mue'] = beta
+
+                big_df = test_queries(
+                    stemmed_queries_df=train_queries_df,
+                    query_to_doc_mapping_df=query_to_doc_mapping_df,
+                    all_query_adveserial_dict=adverserial_dict,
+                    collection_dict=cc_dict,
+                    params=params,
+                    processed_docs_path=processed_docs_folder,
+                    model_to_run=model_to_run)
+
+                res_dict = get_score_retrieval_score_for_df(
+                    affix=affix,
+                    big_df=big_df,
+                    qrel_filepath=qrel_filepath,
+                    save_folder=save_folder)
+
+                if res_dict['all']['NDCG@5'] > best_ndcg:
+                    print(affix + " " + "SCORE : " + str(res_dict['all']))
+                    best_ndcg = res_dict['all']['NDCG@5']
+                    best_config_dict = params.copy()
+                    print(best_config_dict)
+
+            test_fold_df = test_queries(
+                stemmed_queries_df=test_queries_df,
+                query_to_doc_mapping_df=query_to_doc_mapping_df,
+                all_query_adveserial_dict=adverserial_dict,
+                collection_dict=cc_dict,
+                params=best_config_dict,
+                processed_docs_path=processed_docs_folder,
+                model_to_run=model_to_run)
+
+            all_folds_df = all_folds_df.append(test_fold_df, ignore_index=True)
+            all_fold_params_summary += str(start_test_q) + '_' + str(end_test_q) + '\t' + str(best_config_dict) + '\n'
+
+
+        curr_file_name = inner_fold + '_LMIR.'  + model_to_run + ".txt"
+        with open(os.path.join(save_folder + 'final_res/', curr_file_name), 'w') as f:
+            f.write(convert_df_to_trec(all_folds_df))
+        with open(os.path.join(save_folder + 'final_res/', curr_file_name.replace('_Results', '_Params')), 'w') as f:
+            f.write(all_fold_params_summary)
