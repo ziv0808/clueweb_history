96fa38d011aa4e4322d882cb92254c44090d1122
Ziv <zivos0808@gmail.com>
2021-01-01 18:49:17 +0200

    added mm features to trans dict

--- utils.py
+++ utils.py
@@ -945,7 +945,15 @@ def create_feature_list_shortcut_string(feat_list):
         'SimClueWeb': 'SCw',
         'BM25Score': 'BM25',
         'BERTScore' : 'BRT',
-        '-Query-SW' : 'nQSw'
+        '-Query-SW' : 'nQSw',
+        'JMPrevWinner': 'MJmPw',
+        'JMPrev3Winners': 'MJmP3w',
+        'JMPrevBestImprove': 'MJmBi',
+        'JMPrev3BestImprove': 'MJmB3i',
+        'DIRPrevWinner': 'MDrPw',
+        'DIRPrev3Winners': 'MDrP3w',
+        'DIRPrevBestImprove': 'MDrBi',
+        'DIRPrev3BestImprove': 'MDrB3i',
     }
     for feat in feat_list:
         ret_str += '_' + feat_short_dict[feat]
