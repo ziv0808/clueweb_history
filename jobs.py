6803d843bffb298fee968dcbb2b0a15111b5fa73
Ziv <zivos0808@gmail.com>
2021-01-23 19:15:00 +0200

    ks uitily

--- jobs.py
+++ jobs.py
@@ -2967,7 +2967,7 @@ def fix_ks_files_and_produce_stats():
         print(dataset)
         df['num'] = 1
         mdf = df[['Query', 'Round', 'IsKS', 'num']].groupby(['Query', 'Round']).sum()
-        mdf['%ks'] = mdf.apply(lambda row: row['IsKS'] / row['num'], axis = 1)
+        mdf['%ks'] = mdf.apply(lambda row: row['IsKS'] / float(row['num']), axis = 1)
         print("Mean % KS Per Round: " + str(mdf['%ks'].mean()))
         print("% Rounds KS > 0 : " + str(len(mdf[mdf['%ks']>0])/ float(len(mdf))))
 
