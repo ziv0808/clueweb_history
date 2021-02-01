b3cde734c78b89e442b81d82fcd7a73020835ad2
Ziv <zivos0808@gmail.com>
2021-02-02 00:56:43 +0200

    added

--- paper_results_fixer.py
+++ paper_results_fixer.py
@@ -209,8 +209,8 @@ if __name__=='__main__':
         'LM DIR': {'Folder': '/mnt/bi-strg3/v/zivvasilisky/ziv/results/basic_lm/',
                    'FileTemplate': '<DatasetName>_0<RoundNum>_LMIR.DIR.txt',
                    'AlsoLQRmv': True},
-        'BM25': {'Folder': '/mnt/bi-strg3/v/zivvasilisky/ziv/results/bm25_model_res/final_res/',
-                 'FileTemplate': '<DatasetName>_0<RoundNum>_BM25_LoO_Results.txt',
+        'BM25': {'Folder': '/mnt/bi-strg3/v/zivvasilisky/ziv/results/lambdamart_res/ret_res/<DatasetNameUpper>_LTR_All_features_Round0<RoundNum>_with_meta.tsvSNL1_BM25_ByMonths_All_LoO_E_FS_L_LMD_SC_TFSm_TFNSm_SCw_BM25_IDF_BRT/',
+                'FileTemplate': '<DatasetNameUpper>_LTR_All_features_Round0<RoundNum>_with_meta.tsvSNL1_BM25_ByMonths_All_LoO_E_FS_L_LMD_SC_TFSm_TFNSm_SCw_BM25_IDF_BRT_MinMax_BM25.txt',
                  'AlsoLQRmv': True},
     }
 
